7057e1b6727c07a7dd726dc5ed517683782cdf9a
Mukhiddin06 <muhiddinbegiboyev@gmail.com>
2024-02-02 20:14:33 +0500

    Initial commit

---
+++ JS/main.js
@@ -0,0 +1,116 @@
+// misol-1
+// let arr = [
+//     {
+//         name:"Abdulloh",
+//         age:21,
+//         status:false
+//     },
+//     {
+//         name:"Botir",
+//         age:18,
+//         status:true
+//     },
+//     {
+//         name:"Shokir",
+//         age:12,
+//         status:false
+//     },
+// ]
+// for(let i = 0; i < arr.length; i++){
+//     if(arr[i].status == false){
+//         console.log(arr[i])
+//     }
+// }
+
+
+
+// misol-2
+// function factorial(num){
+//     if(num === 0){
+//         return 0
+//     }
+//     else{
+//         return num + factorial(num - 1)
+//     }
+// }
+// console.log(factorial(5));
+
+
+
+// misol-3
+// let arr1 = [2, 6, 8, 9, 5]
+// let arr2 = [3, 5, 9, 4]
+// let num = 0
+// for(let i = 0; i < arr1.length; i++){
+//     for (let j = 0; j < arr2.length; j++){
+//         if(arr1[i] == arr2[j]){
+//             console.log(num += 1)
+//         }
+//     }
+// }
+
+
+
+// misol-4
+// let arr1 = [2, 6, 8, 9, 5]
+// let arr2 = [3, 5, 9, 4]
+// let max = arr1[0];
+// for(let i = 0; i < arr1.length; i++){
+//     for (let j = 0; j < arr2.length; j++){
+//         if(max < arr1[i]){
+//             max = arr1[i]
+//         }
+//     }
+//     console.log(max)
+// }
+
+
+
+// misol-5
+// function elFunction(a){
+//     console.log(a);
+//     let num = a - 1
+//     if(num > 0){
+//         elFunction(num)
+//     }
+// }
+// elFunction(7)
+
+
+
+// ---------------------------//
+
+
+// misol-2
+// function factorial(num){
+//     if(num === 0){
+//         return 1
+//     }
+//     else{
+//         return num * factorial(num - 1)
+//     }
+// }
+// console.log(factorial(6));
+
+
+// misol-4
+// function elFunction(a){
+//     console.log(a);
+//     let num = a - 1
+//     if(num > 0){
+//         elFunction(num)
+//     }
+// }
+// elFunction(7)
+
+
+
+// misol-7
+// let arr1 = [2, 6, 8, 9, 5]
+// let max = arr1[0];
+// for(let i = 0; i < arr1.length; i++){
+//     if(max < arr1[i]){
+//          max = arr1[i]
+//     }
+// }
+// console.log(max)
